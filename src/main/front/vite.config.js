d3f88ff6d7cd0cb0a99d0b4f627911d0d6e60120
kimsongie12 <song3078@naver.com>
2024-04-30 22:23:12 +0900

    feat:vite.config.js

--- src/main/front/vite.config.js
+++ src/main/front/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
 })
