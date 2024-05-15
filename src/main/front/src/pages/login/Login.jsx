c05101ba98a1bebe770600c7452d9930cbc36d92
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-05-15 21:55:53 +0900

    feat: css 수정

--- src/main/front/src/pages/login/Login.jsx
+++ src/main/front/src/pages/login/Login.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaUser, FaLock } from "react-icons/fa";
 import PwSearch from "./L-pwSearch";
-import "./Login.css";
+import "../login/Login.css";
 import axios from "axios";
 function Login() {
   const navigate = useNavigate();
@@ -22,78 +22,76 @@ function Login() {
     return true;
   };
   const valid = validation(id, pw);
-  const buttonOnClick = () => {axios.post('/api/login', { email: id, password: pw})
-      .then(response => {
+  const buttonOnClick = () => {
+    axios
+      .post("/api/login", { email: id, password: pw })
+      .then((response) => {
         setMessage(`${response.data} 님 환영합니다!`); // 로그인 성공 시 이메일을 포함한 메시지를 출력
-
       })
-      .catch(error => {
+      .catch((error) => {
         setMessage("로그인 실패");
         console.error(error);
       });
   };
-  console.log(message)
+  console.log(message);
   return (
-      <div className="login">
-        <Link to={"/"}>
-          {" "}
-          <h1>
-            <img src="./images/ico/retil.png" />
-          </h1>
-        </Link>
+    <div className="login">
+      <Link to={"/"}>
+        {" "}
+        <h1>
+          <img src="./images/ico/retil.png" />
+        </h1>
+      </Link>
 
-
-        <div className="input-box">
-          <input
-              type="text"
-              name="id"
-              value={id}
-              onChange={(event) => {
-                setId(event.target.value);
-              }}
-              placeholder="이메일을 입력하세요"
-          />
-          <FaUser className="icon" />
-        </div>
-        <div className="input-box">
-          <input
-              type="password"
-              name="password"
-              value={pw}
-              onChange={(event) => {
-                setPw(event.target.value);
-              }}
-              placeholder="비밀번호를 입력하세요"
-          />
-          <FaLock className="icon" />
-        </div>
-
-        <div className="remember-signup">
-          <label>
-            <input type="checkbox" />
-            로그인 유지
-          </label>
-          <div className="L-signup">
-            <Link to={"/signup"}>회원가입</Link>
-          </div>
-
-        </div>
-        <button
-            className={valid ? "active" : "inactive"}
-            disabled={!valid}
-            onClick={buttonOnClick}
-            type="submit"
-        >
-          로그인
-        </button>
-
-        <div className="forgot-link">
-          <div className="L-pwSearch">
-            <Link to={"/pwSearch"}>비밀번호 찾기</Link>
-          </div>
-        </div>
-
+      <div className="input-box">
+        <input
+          type="text"
+          name="id"
+          value={id}
+          onChange={(event) => {
+            setId(event.target.value);
+          }}
+          placeholder="이메일을 입력하세요"
+        />
+        <FaUser className="icon" />
       </div>
+      <div className="input-box">
+        <input
+          type="password"
+          name="password"
+          value={pw}
+          onChange={(event) => {
+            setPw(event.target.value);
+          }}
+          placeholder="비밀번호를 입력하세요"
+        />
+        <FaLock className="icon" />
+      </div>
+
+      <div className="remember-signup">
+        <label>
+          <input type="checkbox" />
+          로그인 유지
+        </label>
+        <div className="L-signup">
+          <Link to={"/signup"}>회원가입</Link>
+        </div>
+      </div>
+      <button
+        className={valid ? "active" : "inactive"}
+        disabled={!valid}
+        onClick={buttonOnClick}
+        type="submit"
+      >
+        로그인
+      </button>
+
+      <div className="forgot-link">
+        <div className="L-pwSearch">
+          <Link to={"/pwSearch"}>비밀번호 찾기</Link>
+        </div>
+      </div>
+    </div>
   );
 }
 
