03affa34090e733e0abe691f6904c3c68cc109ce
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-05-15 21:55:32 +0900

    feat: import 삭제

--- src/main/front/src/App.jsx
+++ src/main/front/src/App.jsx
@@ -6,7 +6,6 @@ import Firstmain from "./pages/Firstmain";
 import List from "./pages/list/List";
 import Today from "./pages/Today/Today";
 import Gong from "./pages/Gong";
-import Mainp from "./pages/mainprofilpage/Mainp";
 import Memo from "./pages/memo/Memo";
 import Signup from "./pages/signup/L-signup";
 import PwSearch from "./pages/login/L-pwSearch";
@@ -14,32 +13,26 @@ import Repw from "./pages/login/Repw";
 import Fsignup from "./pages/signup/Fsignup";
 import Mypage from "./pages/mainprofilpage/Mypage";
 import Tier from "./pages/tier/Tier";
-import Menubar from "./pages/menubar/Menubar";
-
-
 
 function App() {
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<Firstmain />} />
-                <Route path="login" element={<Login />} />
-                <Route path="gong" element={<Gong />} />
-                <Route path="list" element={<List />} />
-                <Route path="today" element={<Today />} />
-                <Route path="mainp" element={<Mainp />} />
-                <Route path="memo" element={<Memo />} />
-                <Route path="signup" element={<Signup />} />
-                <Route path="pwSearch" element={<PwSearch />} />
-                <Route path="repw" element={<Repw />} />
-                <Route path="fsignup" element={<Fsignup />} />
-                <Route path="mypage" element={<Mypage />} />
-                <Route path="tier" element={<Tier />} />
-                <Route path="menubar" element={<Menubar />} />
-
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Firstmain />} />
+        <Route path="login" element={<Login />} />
+        <Route path="gong" element={<Gong />} />
+        <Route path="list" element={<List />} />
+        <Route path="today" element={<Today />} />
+        <Route path="memo" element={<Memo />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="pwSearch" element={<PwSearch />} />
+        <Route path="repw" element={<Repw />} />
+        <Route path="fsignup" element={<Fsignup />} />
+        <Route path="mypage" element={<Mypage />} />
+        <Route path="tier" element={<Tier />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
