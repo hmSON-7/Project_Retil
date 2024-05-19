12ea51a913e7f20167141ca42c98c8fcd342a929
j4821_s <sonhm48021@gmail.com>
2024-05-19 21:44:58 +0900

    fix : JWT 누락 코드 추가 및 로그인 오류 해결

--- src/main/java/com/project/Retil/userAccount/Repository/UserRepository.java
+++ src/main/java/com/project/Retil/userAccount/Repository/UserRepository.java
@@ -4,10 +4,12 @@ import com.project.Retil.userAccount.Entity.User_Information;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User_Information, Long> {
 
-    User_Information findByEmail(String email);
+    Optional<User_Information> findByEmail(String email);
 
     boolean existsByEmail(String email);
 }
