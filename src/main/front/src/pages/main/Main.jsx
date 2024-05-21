b173af6c8e5eb50f44d1e0fa2ab6c62679b5e0c9
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-05-21 21:36:26 +0900

    feat:메인페이지 추가

---
+++ src/main/front/src/pages/main/Main.jsx
@@ -0,0 +1,25 @@
+import "./Main.css";
+import Mainp from "../mainprofilpage/Mainp";
+import MainProfile from "./MainProfile";
+import MainGraph from "./MainGraph";
+const Main = () => {
+  return (
+    <div className="main_content">
+      <div className="main_Menubar">
+        <Mainp />
+      </div>
+      <div className="main_top">
+        <div className="main_graph">
+          <span>내 진행사항</span>
+          <MainGraph />
+        </div>
+        <div className="main_profile">
+          <span>내 목표</span>
+          <MainProfile />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
