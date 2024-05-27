ccf622c5deb8c37c37abaff2e83fe616496238f4
j4821_s <sonhm48021@gmail.com>
2024-05-27 20:33:41 +0900

    test: 로그인 연동 및 토큰 테스트

--- src/main/front/src/pages/signup/L-signup.jsx
+++ src/main/front/src/pages/signup/L-signup.jsx
@@ -22,7 +22,7 @@ function Signup() {
   const [showPrivacyModal, setShowPrivacyModal] = useState(false);
 
   const navigate = useNavigate();
-  
+
   const validateEmail = (email) => {
     const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return regex.test(email);
@@ -52,12 +52,14 @@ function Signup() {
     e.preventDefault();
     if (nickname && email && validateEmail(email) && password && confirmPassword && passwordMatch && agreeTerms && agreePrivacy) {
       try {
-        const response = await axios.post('http://localhost:8080/api/users/join', {
-          nickname,
-          email,
-          password,
+        const response = await axios.post('http://localhost:8080/users/join', {
+          nickname: nickname,
+          email: email,
+          password: password,
+          confirmPassword: confirmPassword
+
         });
-        
+
         console.log('회원가입 요청을 보냅니다.', response.data);
 
         if (response.data && response.data.token) {
@@ -133,129 +135,129 @@ function Signup() {
   };
 
   return (
-    <div className="signup-container">
-      <div className="signup-header">
-        <Link to={"/"}>
-          <img className="logo-image" src="/images/ico/retil.png" alt="로고" />
-        </Link>
-      </div>
-      <div className="signup-form">
-        <form className="signup-details" onSubmit={handleSubmit}>
-          <div className={`input-container ${emailError ? 'error' : ''}`} id="nickname-container">
-            <label htmlFor="nickname" className="input-title">
-              닉네임
-            </label>
-            <div className="input-with-icon">
-              <img className="input-icon" src={userIcon} alt="사용자 아이콘" />
-              <input
-                type="text"
-                id="nickname"
-                placeholder="닉네임을 입력해 주세요"
-                value={nickname}
-                onChange={handleNicknameChange}
-              />
-            </div>
-          </div>
-          <div className={`input-container ${emailError ? 'error' : ''}`} id="email-container">
-            <label htmlFor="email" className="input-title">
-              이메일
-            </label>
-            <div className="input-with-icon">
-              <img className="input-icon" src={emailIcon} alt="이메일 아이콘" />
-              <input
-                type="text"
-                id="email"
-                placeholder="예) retil@retil.co.kr"
-                value={email}
-                onChange={handleEmailChange}
-              />
-            </div>
-            {emailError && <div className="input-Emailerror">{emailError}</div>}
-          </div>
-          <div className="input-container" id="password-container">
-            <label htmlFor="password" className="input-title">
-              비밀번호
-            </label>
-            <div className="input-with-icon">
-              <img className="input-icon" src={passwordIcon} alt="비밀번호 아이콘" />
-              <input
-                type="password"
-                id="password"
-                placeholder="영문자, 숫자, 특수문자 포함 최소 8~20자"
-                value={password}
-                onChange={handlePasswordChange}
-              />
-            </div>
-            {passwordError && <div className="input-error">{passwordError}</div>}
-          </div>
-          <div className="input-container" id="confirmPassword-container">
-            <div className="input-with-icon">
-              <img className="input-icon" src={passwordIcon} alt="비밀번호 아이콘" />
-              <input
-                type="password"
-                id="confirmPassword"
-                placeholder="비밀번호를 확인해주세요"
-                value={confirmPassword}
-                onChange={handleConfirmPasswordChange}
-              />
-            </div>
-            {confirmPassword && (
-              <div className={passwordMatch ? 'password-match' : 'password-mismatch'}>
-                {passwordMatch ? '비밀번호가 일치합니다.' : '비밀번호가 일치하지 않습니다.'}
+      <div className="signup-container">
+        <div className="signup-header">
+          <Link to={"/"}>
+            <img className="logo-image" src="/images/ico/retil.png" alt="로고" />
+          </Link>
+        </div>
+        <div className="signup-form">
+          <form className="signup-details" onSubmit={handleSubmit}>
+            <div className={`input-container ${emailError ? 'error' : ''}`} id="nickname-container">
+              <label htmlFor="nickname" className="input-title">
+                닉네임
+              </label>
+              <div className="input-with-icon">
+                <img className="input-icon" src={userIcon} alt="사용자 아이콘" />
+                <input
+                    type="text"
+                    id="nickname"
+                    placeholder="닉네임을 입력해 주세요"
+                    value={nickname}
+                    onChange={handleNicknameChange}
+                />
               </div>
-            )}
-          </div>
-          <button
-            className={`signup-button ${nickname && email && validateEmail(email) && password && confirmPassword && passwordMatch && agreeTerms && agreePrivacy ? 'active' : ''}`}
-            type="submit"
-            disabled={!nickname || !email || !validateEmail(email) || !password || !confirmPassword || !passwordMatch || !agreeTerms || !agreePrivacy || !!passwordError}
-          >
-            회원가입
-          </button>
-          <div className="terms-agreement">
-            <div className="terms-checkbox">
-              <input type="checkbox" id="agreeAll" checked={agreeAll} onChange={handleAgreeAll} />
-              <label htmlFor="agreeAll">전체동의</label>
             </div>
-            <div className="terms-checkbox">
-              <input type="checkbox" id="agreeTerms" checked={agreeTerms} onChange={handleAgreeTerms} />
-              <label htmlFor="agreeTerms">
-                이용약관
-                <Link to="#" onClick={() => setShowTermsModal(true)}>
-                  [보기]
-                </Link>
+            <div className={`input-container ${emailError ? 'error' : ''}`} id="email-container">
+              <label htmlFor="email" className="input-title">
+                이메일
               </label>
+              <div className="input-with-icon">
+                <img className="input-icon" src={emailIcon} alt="이메일 아이콘" />
+                <input
+                    type="text"
+                    id="email"
+                    placeholder="예) retil@retil.co.kr"
+                    value={email}
+                    onChange={handleEmailChange}
+                />
+              </div>
+              {emailError && <div className="input-Emailerror">{emailError}</div>}
             </div>
-            <div className="terms-checkbox">
-              <input type="checkbox" id="agreePrivacy" checked={agreePrivacy} onChange={handleAgreePrivacy} />
-              <label htmlFor="agreePrivacy">
-                개인정보 취급방침
-                <Link to="#" onClick={() => setShowPrivacyModal(true)}>
-                  [보기]
-                </Link>
+            <div className="input-container" id="password-container">
+              <label htmlFor="password" className="input-title">
+                비밀번호
               </label>
+              <div className="input-with-icon">
+                <img className="input-icon" src={passwordIcon} alt="비밀번호 아이콘" />
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="영문자, 숫자, 특수문자 포함 최소 8~20자"
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+              </div>
+              {passwordError && <div className="input-error">{passwordError}</div>}
             </div>
+            <div className="input-container" id="confirmPassword-container">
+              <div className="input-with-icon">
+                <img className="input-icon" src={passwordIcon} alt="비밀번호 아이콘" />
+                <input
+                    type="password"
+                    id="confirmPassword"
+                    placeholder="비밀번호를 확인해주세요"
+                    value={confirmPassword}
+                    onChange={handleConfirmPasswordChange}
+                />
+              </div>
+              {confirmPassword && (
+                  <div className={passwordMatch ? 'password-match' : 'password-mismatch'}>
+                    {passwordMatch ? '비밀번호가 일치합니다.' : '비밀번호가 일치하지 않습니다.'}
+                  </div>
+              )}
+            </div>
+            <button
+                className={`signup-button ${nickname && email && validateEmail(email) && password && confirmPassword && passwordMatch && agreeTerms && agreePrivacy ? 'active' : ''}`}
+                type="submit"
+                disabled={!nickname || !email || !validateEmail(email) || !password || !confirmPassword || !passwordMatch || !agreeTerms || !agreePrivacy || !!passwordError}
+            >
+              회원가입
+            </button>
+            <div className="terms-agreement">
+              <div className="terms-checkbox">
+                <input type="checkbox" id="agreeAll" checked={agreeAll} onChange={handleAgreeAll} />
+                <label htmlFor="agreeAll">전체동의</label>
+              </div>
+              <div className="terms-checkbox">
+                <input type="checkbox" id="agreeTerms" checked={agreeTerms} onChange={handleAgreeTerms} />
+                <label htmlFor="agreeTerms">
+                  이용약관
+                  <Link to="#" onClick={() => setShowTermsModal(true)}>
+                    [보기]
+                  </Link>
+                </label>
+              </div>
+              <div className="terms-checkbox">
+                <input type="checkbox" id="agreePrivacy" checked={agreePrivacy} onChange={handleAgreePrivacy} />
+                <label htmlFor="agreePrivacy">
+                  개인정보 취급방침
+                  <Link to="#" onClick={() => setShowPrivacyModal(true)}>
+                    [보기]
+                  </Link>
+                </label>
+              </div>
+            </div>
+          </form>
+        </div>
+        <Modal isOpen={showTermsModal} onClose={() => setShowTermsModal(false)}>
+          <div className="modal-content">
+            <p>이용약관 내용을 여기에 넣어주세요.</p>
           </div>
-        </form>
+        </Modal>
+        <Modal isOpen={showPrivacyModal} onClose={() => setShowPrivacyModal(false)}>
+          <div className="modal-content">
+            <p>
+              리틸 개인정보 수집 이용 동의 주식회사 OO은 리틸 서비스를 이용하는 귀하의 개인정보를 아래와 같이 수집∙이용합니다. 자세한 내용은 리틸 개인정보 처리방침에서 확인하실 수 있습니다.
+            </p>
+            <ul>
+              <li>수집하는 개인정보 항목 및 이용 목적</li>
+              <li>개인정보 보유 및 이용기간</li>
+              <li>개인정보 수집 및 이용 동의를 거부할 권리</li>
+            </ul>
+          </div>
+        </Modal>
       </div>
-      <Modal isOpen={showTermsModal} onClose={() => setShowTermsModal(false)}>
-        <div className="modal-content">
-          <p>이용약관 내용을 여기에 넣어주세요.</p>
-        </div>
-      </Modal>
-      <Modal isOpen={showPrivacyModal} onClose={() => setShowPrivacyModal(false)}>
-        <div className="modal-content">
-          <p>
-            리틸 개인정보 수집 이용 동의 주식회사 OO은 리틸 서비스를 이용하는 귀하의 개인정보를 아래와 같이 수집∙이용합니다. 자세한 내용은 리틸 개인정보 처리방침에서 확인하실 수 있습니다.
-          </p>
-          <ul>
-            <li>수집하는 개인정보 항목 및 이용 목적</li>
-            <li>개인정보 보유 및 이용기간</li>
-            <li>개인정보 수집 및 이용 동의를 거부할 권리</li>
-          </ul>
-        </div>
-      </Modal>
-    </div>
   );
 }
 
