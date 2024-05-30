7dd490877c6ad1929cdb733e713f2e8ccdc4cfb4
kimsongie12 <song3078@naver.com>
2024-05-30 13:33:24 +0900

    feat:탭 내용 담는곳

---
+++ src/main/front/src/pages/list/TablistContent.jsx
@@ -0,0 +1,15 @@
+//탭 리스트 내용 업로드
+import React from 'react';
+import "./TablistContent.css";
+
+const TablistContent = () => {
+ 
+
+  return (
+    <div className="menubar">
+      <h1>My Website3</h1>
+    </div>
+  );
+};
+
+export default TablistContent;
