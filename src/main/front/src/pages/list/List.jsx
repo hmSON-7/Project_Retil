d4ed5b97d8c9f707758077a21d8caba30a2dbf11
kimsongie12 <song3078@naver.com>
2024-05-30 13:31:27 +0900

    feat:list

--- src/main/front/src/pages/list/List.jsx
+++ src/main/front/src/pages/list/List.jsx
@@ -1,25 +1,15 @@
-import React, { useState } from 'react';
-import './List.css'; // CSS 파일 import
+//list 대표 컴포넌트
+import React from 'react';
+import Tab from './Tab';
+import Tablist from './TablistContent';
+import Mainp from "../mainprofilpage/Mainp";
 
 function List() {
-  const [activeTab, setActiveTab] = useState('tabmenu01');
-
-  const handleTabClick = (tabId) => {
-    setActiveTab(tabId);
-  };
-
   return (
-    <div className="tabcontent">
-      <input type="radio" name="tab" checked={activeTab === 'tabmenu01'} id="tabmenu01" onChange={() => handleTabClick('tabmenu01')} />
-      <label htmlFor="tabmenu01">tab01</label>
-      <input type="radio" name="tab" checked={activeTab === 'tabmenu02'} id="tabmenu02" onChange={() => handleTabClick('tabmenu02')} />
-      <label htmlFor="tabmenu02">tab02</label>
-      <input type="radio" name="tab" checked={activeTab === 'tabmenu03'} id="tabmenu03" onChange={() => handleTabClick('tabmenu03')} />
-      <label htmlFor="tabmenu03">tab03</label>
-
-      <div className={`content-box content01 ${activeTab === 'tabmenu01' ? 'active' : ''}`}>content01</div>
-      <div className={`content-box content02 ${activeTab === 'tabmenu02' ? 'active' : ''}`}>content02</div>
-      <div className={`content-box content03 ${activeTab === 'tabmenu03' ? 'active' : ''}`}>content03</div>
+    <div>
+      <Mainp />
+      <Tab />
+      
     </div>
   );
 }
