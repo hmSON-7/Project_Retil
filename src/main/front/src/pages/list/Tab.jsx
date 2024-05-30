8cd19c953bfa6e5e653f067147cab983159d0d99
kimsongie12 <song3078@naver.com>
2024-05-30 15:18:19 +0900

    feat:Tab

--- src/main/front/src/pages/list/Tab.jsx
+++ src/main/front/src/pages/list/Tab.jsx
@@ -15,6 +15,7 @@ const Tab = () => {
   
   // 모달 창의 열림 상태를 관리하는 상태
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [showMoreTabs, setShowMoreTabs] = useState(false);
 
   // 탭 선택 핸들러: 클릭한 탭의 인덱스를 currentTab 상태로 설정
   const selectMenuHandler = (index) => {
@@ -37,29 +38,42 @@ const Tab = () => {
     setMenuArr([...menuArr, { name: newTabName, content: newTabContent }]);
     setIsModalOpen(false); // 모달 창을 닫음
   };
+   // "더 보기" 버튼 클릭 핸들러: 더 많은 탭을 보이도록 설정하거나 다시 숨김
+  const handleShowMoreTabs = () => {
+    setShowMoreTabs(!showMoreTabs);
+  };
+
 
   return (
     <div className='tabul'>
+      {/* 탭 목록을 표시합니다. */}
       <ul className="tab-menu">
-        {/* menuArr 배열을 map 함수로 순회하여 탭 메뉴를 렌더링 */}
-        {menuArr.map((el, index) => (
-          <li
+        {/* 보여질 탭은 3개까지만, showMoreTabs가 true인 경우 모두 표시합니다. */}
+        {menuArr.slice(0, showMoreTabs ? menuArr.length : 3).map((el, index) => (
+          <div
             key={index}
             className={index === currentTab ? 'submenu focused' : 'submenu'}
             onClick={() => selectMenuHandler(index)}
           >
             {el.name}
-          </li>
+          </div>
         ))}
-        {/* "추가하기" 버튼을 클릭하면 모달 창이 열림 */}
-        <li className="submenubutton" onClick={openModalHandler}>
+        {/* 탭이 3개 이상인 경우 "더 보기" 버튼을 표시합니다. */}
+        {menuArr.length > 3 && (
+          <div className="submenubutton" onClick={handleShowMoreTabs}>
+            {showMoreTabs ? '접기' : '더 보기'}
+          </div>
+        )}
+        {/* "추가하기" 버튼을 표시합니다. */}
+        <div className="submenubutton" onClick={openModalHandler}>
           추가하기
-        </li>
+        </div>
       </ul>
+      {/* 현재 선택된 탭의 내용을 표시합니다. */}
       <div className="desc">
         <p>{menuArr[currentTab].content}</p>
       </div>
-      {/* 모달 컴포넌트 렌더링: isOpen, onClose, onAddTab 속성을 전달 */}
+      {/* 모달 컴포넌트를 렌더링합니다. isOpen, onClose, onAddTab 속성을 전달합니다. */}
       <Listmodal isOpen={isModalOpen} onClose={handleModalClose} onAddTab={handleAddTab} />
     </div>
   );
