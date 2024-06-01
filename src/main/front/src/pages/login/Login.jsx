daa89b1656284043ba8d0487aaf9bafa566f4da1
hobinBae <123109572+hobinBae@users.noreply.github.com>
2024-06-01 20:13:18 +0900

    feat:엔터 누르면 로그인 수정

--- src/main/front/src/pages/login/Login.jsx
+++ src/main/front/src/pages/login/Login.jsx
@@ -1,109 +1,115 @@
-import React from "react";
-//import { Route,Routes } from "react-router-dom";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaUser, FaLock } from "react-icons/fa";
-import PwSearch from "./L-pwSearch";
-import "./Login.css";
 import axios from "axios";
+import "./Login.css";
+
 function Login() {
-  const navigate = useNavigate();
-  const [id, setId] = useState("");
-  const [pw, setPw] = useState("");
-  const [message, setMessage] = useState("");
-  const validation = (idText, pwText) => {
-    if (!idText.includes("@")) {
-      return false;
-    }
-    if (pwText.length < 8) {
-      return false;
-    }
+    const navigate = useNavigate();
+    const [id, setId] = useState("");
+    const [pw, setPw] = useState("");
+    const [message, setMessage] = useState("");
 
-    return true;
-  };
-  const valid = validation(id, pw);
-  const buttonOnClick = () => {
-    axios
-      .post(`http://localhost:8080/users/login`, { email: id, password: pw })
-      .then((response) => {
-        const { token, id } = response.data; // 서버에서 받은 토큰
-        console.log(response.data);
-        // 토큰을 로컬 스토리지에 저장
-        localStorage.setItem("token", token);
-        localStorage.setItem("user_id", id);
-        console.log(id);
+    const validation = (idText, pwText) => {
+        if (!idText.includes("@")) {
+            return false;
+        }
+        if (pwText.length < 8) {
+            return false;
+        }
+        return true;
+    };
 
-        // 환영 메시지 설정
-        setMessage("로그인 성공!");
+    const valid = validation(id, pw);
 
-        // 마이페이지로 이동
-        navigate("/main");
-      })
-      .catch((error) => {
-        const errorMsg = error.response ? error.response.data : "로그인 실패";
-        setMessage(errorMsg);
-        console.error(error);
-      });
-  };
-  return (
-    <div className="login">
-      <Link to={"/"}>
-        {" "}
-        <h1>
-          <img src="./images/ico/retil.png" />
-        </h1>
-      </Link>
+    const buttonOnClick = () => {
+        axios
+            .post(`http://localhost:8080/users/login`, { email: id, password: pw })
+            .then((response) => {
+                const { token, id } = response.data; // 서버에서 받은 토큰
+                // 토큰을 로컬 스토리지에 저장
+                localStorage.setItem("token", token);
+                localStorage.setItem("user_id", id);
+                navigate("/main");
+            })
+            .catch((error) => {
+                const errorMsg = error.response ? error.response.data : "로그인 실패";
+                setMessage(errorMsg);
+                console.error(error);
+            });
+    };
 
-      <div className="input-box">
-        <input
-          type="text"
-          name="id"
-          value={id}
-          onChange={(event) => {
-            setId(event.target.value);
-          }}
-          placeholder="이메일을 입력하세요"
-        />
-        <FaUser className="icon" />
-      </div>
-      <div className="input-box">
-        <input
-          type="password"
-          name="password"
-          value={pw}
-          onChange={(event) => {
-            setPw(event.target.value);
-          }}
-          placeholder="비밀번호를 입력하세요"
-        />
-        <FaLock className="icon" />
-      </div>
+    const handleFormSubmit = (event) => {
+        event.preventDefault(); // 폼의 기본 동작을 막음 (새로고침 방지)
+        if (valid) {
+            buttonOnClick(); // 로그인 버튼 클릭 함수 호출
+        }
+    };
 
-      <div className="remember-signup">
-        <label>
-          <input type="checkbox" />
-          로그인 유지
-        </label>
-        <div className="L-signup">
-          <Link to={"/signup"}>회원가입</Link>
+    return (
+        <div className="login">
+            <Link to={"/"}>
+                {" "}
+                <h1>
+                    <img src="./images/ico/retil.png" alt="Retil Logo" />
+                </h1>
+            </Link>
+
+            <form onSubmit={handleFormSubmit}>
+                <div className="input-box">
+                    <input
+                        type="text"
+                        name="id"
+                        value={id}
+                        onChange={(event) => {
+                            setId(event.target.value);
+                        }}
+                        placeholder="이메일을 입력하세요"
+                    />
+                    <FaUser className="icon" />
+                </div>
+
+                <div className="input-box">
+                    <input
+                        type="password"
+                        name="password"
+                        value={pw}
+                        onChange={(event) => {
+                            setPw(event.target.value);
+                        }}
+                        placeholder="비밀번호를 입력하세요"
+                    />
+                    <FaLock className="icon" />
+                </div>
+
+                <div className="remember-signup">
+                    <label>
+                        <input type="checkbox" />
+                        로그인 유지
+                    </label>
+                    <div className="L-signup">
+                        <Link to={"/signup"}>회원가입</Link>
+                    </div>
+                </div>
+
+                <button
+                    className={valid ? "active" : "inactive"}
+                    disabled={!valid}
+                    type="submit"
+                >
+                    로그인
+                </button>
+            </form>
+
+            <div className="forgot-link">
+                <div className="L-pwSearch">
+                    <Link to={"/pwSearch"}>비밀번호 찾기</Link>
+                </div>
+            </div>
+
+            {message && <p className="message">{message}</p>}
         </div>
-      </div>
-      <button
-        className={valid ? "active" : "inactive"}
-        disabled={!valid}
-        onClick={buttonOnClick}
-        type="submit"
-      >
-        로그인
-      </button>
-
-      <div className="forgot-link">
-        <div className="L-pwSearch">
-          <Link to={"/pwSearch"}>비밀번호 찾기</Link>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Login;
